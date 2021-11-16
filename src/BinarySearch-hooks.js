b3e7c61850c56549b7ec24cc149fea6aeb83abfd
John Bohannon <imjohnbo@github.com>
2021-11-16 01:43:02 +0000

    fix: state bug in hooks

--- src/BinarySearch-hooks.js
+++ src/BinarySearch-hooks.js
@@ -4,17 +4,16 @@ import { buildBinarySearchState } from './helpers';
 
 function BinarySearch(props) {
 
-  const [step, setStep] = useState({});
+  const [step, setStep] = useState(0);
+  const states = buildBinarySearchState(props.array, props.searchElement);
 
   useEffect(() => {
-    const states = buildBinarySearchState(props.array, props.searchElement);
-    let stateIndex = 0;
     const timerID = setInterval(incrementState, props.delayMS);
 
     function incrementState() {
-      console.log('hey', stateIndex, states);
-      if (stateIndex < states.length) {
-        tick(states[stateIndex++]);
+      if (step < states.length - 1) {
+        const newStep = step + 1;
+        setStep(newStep);
       }
       else {
         clearInterval(timerID)
@@ -24,25 +23,21 @@ function BinarySearch(props) {
     return () => {
       clearInterval(timerID);
     };
-  }, [props.array, props.delayMS, props.searchElement]);
-
-  function tick(s) {
-    setStep(s);
-  }
+  });
 
   function getArrayRow() {
     return props.array.map((value, index) =>
-      <div className={`block ${step.found === index ? 'found' : ''}`} key={index}>{value}</div>
+      <div className={`block ${states[step].found === index ? 'found' : ''}`} key={index}>{value}</div>
     );
   }
 
   function getDescriptionRow() {
     return props.array.map((value, index) =>
       <p key={index}>
-        {step.low === index ? 'low ⬆️ ' : ''}
-        {step.mid === index ? 'mid ⬆️ ' : ''}
-        {step.high === index ? 'high ⬆️ ' : ''}
-        {step.found === index ? 'found ⬆️ ' : ''}
+        {states[step].low === index ? 'low ⬆️ ' : ''}
+        {states[step].mid === index ? 'mid ⬆️ ' : ''}
+        {states[step].high === index ? 'high ⬆️ ' : ''}
+        {states[step].found === index ? 'found ⬆️ ' : ''}
       </p>
     );
   }
